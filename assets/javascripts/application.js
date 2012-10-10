ae36e2d1e6ac60c1f98c80b5a0da764c5444aa12
Craig Coles <craigc@wearebeef.co.uk>
2012-10-10 10:09:20 +0100

    Uncommented and formatted the application.js file

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,84 +1,79 @@
-/* `Application JS
-----------------------------------------------------------------------------------------------------*/
+/* ----- Application JS ----- */
 
-/* `Cufon */
-// Cufon.replace('', {
-//   hover: 'true',
-//   fontFamily: ''
-// });
+  /* ----- Cufon----- */
+  Cufon.replace('', {
+    hover: 'true',
+    fontFamily: ''
+  });
 
+  /* ----- SWFObject----- */
+  var flashvars = {};
+  var params = {};
+  var attributes = {};
+  swfobject.embedSWF("/flash/", "flash-anni", "420", "90", "9.0.0", flashvars, params, attributes);
 
-/* `SWFObject */
-// var flashvars = {};
-// var params = {};
-// var attributes = {};
-// swfobject.embedSWF("/flash/", "flash-anni", "420", "90", "9.0.0", flashvars, params, attributes);
+  /* ----- jQuery Start----- */
+  $(document).ready(function () {
+    // STYLING FIX'S
+    // IE FIX'S
+  });
 
-/* `jQuery Start */
-$(document).ready(function () {
+/* ----- Common Functions / Tools / Plugins ----- */
 
-// STYLING FIX'S
-// IE FIX'S
+  /* ----- Slide Show ----- */
+  $(function(){
+    $('.hero-panel').slides({
+    preload: true,
+    preloadImage: '/images/loading.gif',
+    effect:'slide',
+    hoverPause: false,
+    generatePagination: false,
+    slideSpeed: 800,
+    generateNextPrev: true
+    });
+    $('.js-needed').hide();
+  });
 
-}); //eo: Document Ready
-
-/* `Common Functions / Tools / Plugins
-----------------------------------------------------------------------------------------------------*/
-
-/* `Slides.js Slide Show */
-// $(function(){
-//   $('.hero-panel').slides({
-//   preload: true,
-//   preloadImage: '/images/loading.gif',
-//   effect:'slide',
-//   hoverPause: false,
-//   generatePagination: false,
-//   slideSpeed: 800,
-//   generateNextPrev: true
-//   });
-//   $('.js-needed').hide();
-// });
-
-/* `Adding body images */
-// $("#body a.lightbox").each(function(){
-//   $(this).append('<span class="overlay">&nbsp;</span>');
-//  });
+  /* ----- Adding Body Images ----- */
+  $("#body a.lightbox").each(function(){
+    $(this).append('<span class="overlay">&nbsp;</span>');
+   });
  
-// $("#body a").each(function(){
-//   image = $(this);
-//   image.wrap('<a class="lightbox image-wrap" target="_blank" title="'+ image.attr('title') +'" href="'+   image.attr('longdesc') +'" />');
-//   image.after('<span class="zoom">&nbsp;</span>');
-// });
+  $("#body a").each(function(){
+    image = $(this);
+    image.wrap('<a class="lightbox image-wrap" target="_blank" title="'+ image.attr('title') +'" href="'+   image.attr('longdesc') +'" />');
+    image.after('<span class="zoom">&nbsp;</span>');
+  });
 
-/* `Fancybox */
-// $('').fancybox({
-//   'padding'        : 10,
-//   'autoScale'      : true,
-//   'transitionIn'    : 'elastic',
-//   'transitionOut'    : 'elastic',
-//   'title'          : false,
-//   'width'        : 680,
-//   'height'        : 495,
-//   'centerOnScroll'   : true,
-//   'titlePosition' : 'over'
-// });
+  /* ----- Fancybox ----- */
+  $('').fancybox({
+    'padding'        : 10,
+    'autoScale'      : true,
+    'transitionIn'    : 'elastic',
+    'transitionOut'    : 'elastic',
+    'title'          : false,
+    'width'        : 680,
+    'height'        : 495,
+    'centerOnScroll'   : true,
+    'titlePosition' : 'over'
+  });
 
-/* `JCarousel Lite */
-// $(".component.gallery.single.wide .carousel").each(function(index, gallery) {
-//   gallery = $(gallery);
-//   if (gallery.find('li').length > 3) {
-//           
-//     gallery.parents(".carousel-wrap").css("margin","10px 70px 0 50px");
-//     gallery.before("<a class='carousel-back'>Backward</a>");
-//     gallery.after("<a class='carousel-forward'>Forward</a>");
-// 
-//     gallery.jCarouselLite({
-//       btnNext: gallery.next(".carousel-forward"),
-//       btnPrev: gallery.prev(".carousel-back"),
-//       visible: 3
-//     });
-//   }
-// });
+  /* ----- JCarousel Lite ----- */
+  $(".component.gallery.single.wide .carousel").each(function(index, gallery) {
+    gallery = $(gallery);
+    if (gallery.find('li').length > 3) {
+          
+      gallery.parents(".carousel-wrap").css("margin","10px 70px 0 50px");
+      gallery.before("<a class='carousel-back'>Backward</a>");
+      gallery.after("<a class='carousel-forward'>Forward</a>");
 
-/* `Flash Messages */
-// $('#flash').delay(4000).slideUp(400);
+      gallery.jCarouselLite({
+        btnNext: gallery.next(".carousel-forward"),
+        btnPrev: gallery.prev(".carousel-back"),
+        visible: 3
+      });
+    }
+  });
+
+  /* ----- Flash Messages ----- */
+  $('#flash').delay(4000).slideUp(400);
